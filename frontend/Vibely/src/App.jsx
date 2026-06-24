897f0e9445646b27859badadb4b3fe07ad81cc46
Srishti Srivastava <srishti.school2010@gmail.com>
2026-06-24 22:24:00 +0530

    feat: Add About section and footer to enhance site structure and styling.

--- frontend/Vibely/src/App.jsx
+++ frontend/Vibely/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Features from "./components/Features.jsx";
+import About from "./components/about.jsx";
 
 function App() {
   return (
@@ -9,6 +11,32 @@ function App() {
       <Navbar />
       <Hero />
       <Features />
+      <hr className="line" />
+      <About />
+
+      <footer className="footer">
+        <ul className="footer-list">
+          <li className="footer-l">
+            <a href="#about">About</a>
+          </li>
+          <li className="footer-l">
+            <a href="#features">Features</a>
+          </li>
+          <Link to="/login" className="footer-n">
+            Login
+          </Link>
+          <Link to="/signup" className="footer-n">
+            Sign Up
+          </Link>
+        </ul>
+
+        <p className="footer-para">&copy; 2026 Vibely. All rights reserved.</p>
+
+        <p className="git">
+          Made with ❤️ by
+          <a href="https://github.com/SrishtiOnGit/Vibely">Srishti</a>
+        </p>
+      </footer>
     </>
   );
 }
---
+++ frontend/Vibely/src/components/about.jsx
@@ -0,0 +1,17 @@
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1 className="about-title">About Vibely</h1>
+
+      <p className="about-para">
+        This is a music platform I built to make discovering songs easier and
+        more personal. It suggests music based on mood and taste, and helps you
+        create playlists without any hassle.
+      </p>
+    </div>
+  );
+};
+
+export default About;
